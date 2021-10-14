f15fc87161716e384b98f92a0ceeb7112930d42f
amart3 <46977916+amart3@users.noreply.github.com>
2021-10-14 10:47:04 -0500

    Program 3 = Program 4

---
+++ Program4/Prog4Lam0097SU21/Prog4Lam0097SU21/ToDoTableViewCell.swift
@@ -0,0 +1,28 @@
+//  Lawrence "Alex" Martin
+//  Lam0097 - 903986867
+//  Comp 5970 - Dr. Chapman
+//  ToDoTableViewCell.swift
+//  Prog4Lam0097SU21
+//
+//  Created by user197928 on 7/9/21.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+    //Allow task cell to implement on multiple controllers.
+    @IBOutlet weak var taskLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
